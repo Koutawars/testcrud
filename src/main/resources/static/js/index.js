5a5253ad348cf38a2e26422fa89ec52a4505db30
Koutawars <admikouta@gmail.com>
2019-03-27 23:25:17 -0500

    crud sin delete

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,4 +1,5 @@
-  
+var editando; 
+
 $(document).ready(function(){
   $('#boton').click(function(e){
 	  e.preventDefault();
@@ -8,16 +9,56 @@ $(document).ready(function(){
 	  let edad = $('#edad').val();
 	  if(nombre != '' && apellido != '' && codigo != '' && edad != ''){
 		  // ajax request
+		  // $.post("/link",data, function(data,status){});
+		  // M.toast({html: 'Inscribiendo...'});
 		  console.log({nombre,apellido,codigo,edad});
+		  agregarTabla(0, nombre, apellido, codigo, edad);
 	  }
 	  
   });
+  
+  $('#editar').click(function(){
+	  let nombre = $('#nombreEdit').val();
+	  let apellido = $('#apellidoEdit').val();
+	  let codigo = $('#codigoEdit').val();
+	  let edad = $('#edadEdit').val();
+	  if(nombre != '' && apellido != '' && codigo != '' && edad != ''){
+		  // Ajax request
+		  // M.toast({html: 'Editando...'});
+		  editarTabla(editando, nombre, apellido, codigo, edad);
+	  }
+  });
+  
   $('select').formSelect();
   $('.modal').modal();
 
 });
-  
+ 
+function editarTabla(id, nombre, apellido, codigo, edad){
+	let markup = `<td>${id}</td><td>${nombre}</td><td>${apellido}</td><td>${codigo}</td><td>${edad}</td><td><a id="btn${id}" class="waves-effect waves-light btn modal-trigger" href="#modal1">Editar</a></td>`;
+	$("tr[name='"+ id +"']").html(markup);
+	$('#btn'+id).click(function(){
+		editando = id;
+		let tds = $("tr[name='"+ id +"'] > td");
+		$('#nombreEdit').val(tds[1].innerHTML);
+		$('#apellidoEdit').val(tds[2].innerHTML);
+		$('#codigoEdit').val(tds[3].innerHTML);
+		$('#edadEdit').val(tds[4].innerHTML);
+		M.updateTextFields();
+	});
+}
+
+
 function agregarTabla(id, nombre, apellido, codigo, edad){
-	
-	//$('#tablaEstudiantes tbody')
+	let markup = `<tr name="${id}"><td>${id}</td><td>${nombre}</td><td>${apellido}</td><td>${codigo}</td><td>${edad}</td><td><a id="btn${id}" class="waves-effect waves-light btn modal-trigger" href="#modal1">Editar</a></td></tr>`;
+	$('#tablaEstudiantes tbody').append(markup);
+	$('#btn'+id).click(function(){
+		editando = id;
+		let tds = $("tr[name='"+ id +"'] > td");
+		$('#nombreEdit').val(tds[1].innerHTML);
+		$('#apellidoEdit').val(tds[2].innerHTML);
+		$('#codigoEdit').val(tds[3].innerHTML);
+		$('#edadEdit').val(tds[4].innerHTML);
+		M.updateTextFields();
+	});
 }
