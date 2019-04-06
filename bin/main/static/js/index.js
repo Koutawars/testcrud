213b48b1b5018beb36fd506931077e12122b7141
Koutawars <admikouta@gmail.com>
2019-04-05 22:03:13 -0500

    delegación de eventos

--- bin/main/static/js/index.js
+++ bin/main/static/js/index.js
@@ -1,21 +1,22 @@
 var editando; 
 
 $(document).ready(function(){
-	let request = $.ajax({
-		  url: "estudiantes",
-		  method: "GET"
-		});
-		 
-	request.done(function(data) {
-		data.forEach(function(e) {
-			agregarTabla(e.id, e.nombre, e.apellido, e.codigo, e.edad);
-		});
+	$.ajax({
+		method: "GET",
+		url: "/estudiantes",
+		datatype: "JSON",
+		success: function(data, status, jqXHR ){
+			data.forEach(function(i){
+				agregarTabla(i.id, i.nombre, i.apellido, i.codigo, i.edad);
+			});
+		},
+		error: function(jqXHR , status, e){
+			M.toast({html: 'Error al traer estudiantes... status: '+jqXHR.status});
+			console.log(jqXHR);
+		}
 	});
-	 
-	request.fail(function( jqXHR, textStatus ) {
-		M.toast({html: 'Falla a traer en la base de datos. code:'+jqXHR.status});
-	});
-		
+	
+	
   $('#boton').click(function(e){
 	  e.preventDefault();
 	  let nombre = $('#nombre').val();
@@ -23,38 +24,33 @@ $(document).ready(function(){
 	  let codigo = $('#codigo').val();
 	  let edad = $('#edad').val();
 	  if(nombre != '' && apellido != '' && codigo != '' && edad != ''){
-		  // ajax request
-		  // $.post("/link",data, function(data,status){});
 		  M.toast({html: 'Inscribiendo...'});
-		  console.log({nombre, apellido, codigo, edad});
+		  // ajax request
+		  
 		  let estudiante = {
-			  nombre,
-			  apellido,
-			  codigo,
-			  edad
-		  }
-		  let request = $.ajax({
-			  url: "estudiantes/add",
-			  data: JSON.stringify(estudiante),
-			  method: "POST",
-			  contentType: "application/json",
-			  dataType: 'json'
-		  });
-			 
-			request.done(function(data) {
-				agregarTabla(data.id, data.nombre, data.apellido, data.codigo, data.edad);
-				M.toast({html: '¡Inscrito!'});
-			});
-			 
-			request.fail(function( jqXHR, textStatus ) {
-				console.log(jqXHR);
-				if(jqXHR.status == 409){
-					M.toast({html: 'Código duplicado, ya existe otro alumno con este codigo.'});
-				}else{
-					M.toast({html: 'Fallo a inscribir. code:'+jqXHR.status});
+					nombre,
+					apellido,
+					codigo,
+					edad
+				};
+		  
+			$.ajax({
+				method: "POST",
+				url: "estudiantes/add",
+				datatype: "JSON",
+				contentType: "application/json",
+				data: JSON.stringify(estudiante),
+				success: function(data, status, jqXHR ){
+					M.toast({html: '¡Inscrito!'});
+					agregarTabla(data.id, data.nombre, data.apellido, data.codigo, data.edad);
+				},
+				error: function(jqXHR , status, e){
+					M.toast({html: 'Error a crear estudiante status: '+jqXHR.status});
+					console.log(jqXHR);
 				}
 			});
 	  }
+	  
   });
   
   $('#editar').click(function(){
@@ -63,31 +59,31 @@ $(document).ready(function(){
 	  let codigo = $('#codigoEdit').val();
 	  let edad = $('#edadEdit').val();
 	  if(nombre != '' && apellido != '' && codigo != '' && edad != ''){
-		  // Ajax request
-		  M.toast({html: 'Editando...'});
+		  // Ajax request actualizar
+		  M.toast({html: 'Actualizando...'});
 		  let estudiante = {
-				  id:editando,
-				  nombre,
-				  apellido,
-				  codigo,
-				  edad
-			  }
-		  let request = $.ajax({
-			  url: "estudiantes/update",
-			  data: JSON.stringify(estudiante),
-			  method: "PUT",
-			  contentType: "application/json",
-			  dataType: 'json'
-		  });
-			 
-		  request.done(function(data) {
-			  editarTabla(data.id, data.nombre, data.apellido, data.codigo, data.edad);
-			  M.toast({html: '¡Editado!'});
-		  });
-			 
-		  request.fail(function( jqXHR, textStatus ) {
-			  M.toast({html: 'Fallo a editar. code:'+jqXHR.status});
-		  });
+				  	id:editando,
+					nombre,
+					apellido,
+					codigo,
+					edad
+				};
+		  
+			$.ajax({
+				method: "PUT",
+				url: "estudiantes/update",
+				datatype: "JSON",
+				contentType: "application/json",
+				data: JSON.stringify(estudiante),
+				success: function(data, status, jqXHR ){
+					M.toast({html: '¡Actualizado!'});
+					editarTabla(data.id, data.nombre, data.apellido, data.codigo, data.edad);
+				},
+				error: function(jqXHR , status, e){
+					M.toast({html: 'Error a crear estudiante status: '+jqXHR.status});
+					console.log(jqXHR);
+				}
+			});
 	  }
   });
   
@@ -99,42 +95,14 @@ $(document).ready(function(){
 function editarTabla(id, nombre, apellido, codigo, edad){
 	let markup = `<td>${id}</td><td>${nombre}</td><td>${apellido}</td><td>${codigo}</td><td>${edad}</td><td><i id="btn${id}" class="small material-icons modal-trigger btn-editar" href="#modal1">edit</i> <i id="del${id}" class="del red-text small material-icons">delete</i></td>`;
 	$("tr[name='"+ id +"']").html(markup);
-	$('#btn'+id).click(function(){
-		editando = id;
-		let tds = $("tr[name='"+ id +"'] > td");
-		$('#nombreEdit').val(tds[1].innerHTML);
-		$('#apellidoEdit').val(tds[2].innerHTML);
-		$('#codigoEdit').val(tds[3].innerHTML);
-		$('#edadEdit').val(tds[4].innerHTML);
-		M.updateTextFields();
-	});
-	$('#del'+id).click(function(){
-		// AJAX Request remove
-		  M.toast({html: 'Eliminando...'});
-		  let request = $.ajax({
-			  url: "estudiantes/delete",
-			  data: JSON.stringify(id),
-			  method: "DELETE",
-			  contentType: "application/json"
-		  });
-		  
-		  request.done(function(data) {
-			  $("tr[name='"+ id +"']").remove();
-			  M.toast({html: '¡Eliminado!'});
-		  });
-			 
-		  request.fail(function( jqXHR, textStatus ) {
-			  M.toast({html: 'Fallo a eliminar. code:'+jqXHR.status});
-			  console.log(jqXHR);
-		  });
-	});
 }
 
 
 function agregarTabla(id, nombre, apellido, codigo, edad){
 	let markup = `<tr name="${id}"><td>${id}</td><td>${nombre}</td><td>${apellido}</td><td>${codigo}</td><td>${edad}</td><td><i id="btn${id}" class="small material-icons modal-trigger btn-editar" href="#modal1">edit</i> <i id="del${id}" class="del red-text small material-icons">delete</i></td></tr>`;
 	$('#tablaEstudiantes tbody').append(markup);
-	$('#btn'+id).click(function(){
+	
+	$("tr[name='"+ id +"']").on('click', '#btn'+id, function() {
 		editando = id;
 		let tds = $("tr[name='"+ id +"'] > td");
 		$('#nombreEdit').val(tds[1].innerHTML);
@@ -143,24 +111,24 @@ function agregarTabla(id, nombre, apellido, codigo, edad){
 		$('#edadEdit').val(tds[4].innerHTML);
 		M.updateTextFields();
 	});
-	$('#del'+id).click(function(){
+	
+	$("tr[name='"+ id +"']").on('click', '#del'+id, function() {
 		// AJAX Request remove
-		  M.toast({html: 'Eliminando...'});
-		  let request = $.ajax({
-			  url: "estudiantes/delete",
-			  data: JSON.stringify(id),
-			  method: "DELETE",
-			  contentType: "application/json"
-		  });
-		  
-		  request.done(function(data) {
-			  $("tr[name='"+ id +"']").remove();
-			  M.toast({html: '¡Eliminado!'});
-		  });
-			 
-		  request.fail(function( jqXHR, textStatus ) {
-			  M.toast({html: 'Fallo a eliminar. code:'+jqXHR.status});
-			  console.log(jqXHR);
-		  });
+		M.toast({html: 'Eliminando...'});
+		$.ajax({
+			method: "DELETE",
+			url: "estudiantes/delete",
+			data: JSON.stringify(id),
+			contentType: "application/json",
+			datatype: "JSON",
+			success: function(data, status, jqXHR ){
+				$("tr[name='"+ id +"']").remove();
+				M.toast({html: '¡Eliminado!'});
+			},
+			error: function(jqXHR , status, e){
+				M.toast({html: 'Error al eliminar status: '+jqXHR.status});
+				console.log(jqXHR);
+			}
+		});
 	});
 }
